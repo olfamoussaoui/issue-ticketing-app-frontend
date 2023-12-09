363d4d34c7d6dfd2ebe2835f73ded2a00c232712
olfamoussaoui <moussaoui.olfa1@gmail.com>
2023-12-09 13:56:26 +0100

    add components and remove tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueDetailComponent } from './issue-detail/issue-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IssueListComponent,
+    IssueDetailComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/issue-detail/issue-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-detail',
+  templateUrl: './issue-detail.component.html',
+  styleUrls: ['./issue-detail.component.css']
+})
+export class IssueDetailComponent {
+
+}
